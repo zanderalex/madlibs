2f3aa583cd036acb958e002fdf7e9ed8ab97ae8d
Alex Martinez <zanderalex377@gmail.com>
2019-03-27 14:36:13 -0700

    link bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
-  $('#blanks').submit(function(event) {
-    $('.person1').append('blah blah');
-    $('.person2').append('blah blah');
-    $('.animal').append('blah blah');
-    $('.exclamation').append('blah blah');
-    $('.verb').append('blah blah');
-    $('.noun').append('blah blah');
+  $("#blanks").submit(function(event) {
+    var person1Input = $('input#person1').val();
+    var person1Input = $('input#person2').val();
+    var person1Input = $('input#animal').val();
+    var person1Input = $('input#exclamation').val();
+    var person1Input = $('input#verb').val();
+    var person1Input = $('input#noun').val();
 
-    $('story').show();
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $('.animal').text(animalInput);
+    $('.exclamation').text(exclamationInput);
+    $('.verb').text(verbInput);
+    $('.noun').text(nounInput);
+
+    $("#story").show();
 
     event.preventDefault();
   });
